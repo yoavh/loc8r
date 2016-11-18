900ee147e8a3590f283d19f9f9c6146423b74f24
yoavh <yoav_hagashi@yahoo.com>
2016-11-18 21:16:04 +0200

    finished chapter 8

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -2,58 +2,25 @@ var request = require('request');
 var apiOptions = {
     server: "http://localhost:3000"
 };
+
 if (process.env.NODE_ENV === 'production') {
     apiOptions.server = "https://getting-mean-loc8r.herokuapp.com";
 }
+
 var renderHomepage = function (req, res, responseBody) {
-    var message;
-    if (!(responseBody instanceof Array)) {
-        message = "API lookup error";
-        responseBody = [];
-    } else {
-        if (!responseBody.length) {
-            message = "No places found nearby";
-        }
-    }
     res.render('locations-list', {
         title: 'Loc8r - find a place to work with wifi',
         pageHeader: {
             title: 'Loc8r',
             strapline: 'Find places to work with wifi near you!'
         },
-        sidebar: "Looking for wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffee, cake or a pint? Let Loc8r help you find the place you're looking for.",
-        locations: responseBody,
-        message: message
+        sidebar: "Looking for wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffee, cake or a pint? Let Loc8r help you find the place you're looking for."
     });
 };
 
-
 /* GET 'home' page */
 module.exports.homelist = function (req, res) {
-    var requestOptions, path;
-    path = '/api/locations';
-    requestOptions = {
-        url: apiOptions.server + path,
-        method: "GET",
-        qs: {
-            //lng: 34.8085703,
-            lng: 34.8085703,
-            //lat: 31.9629033,
-            lat: 31.9629032,
-            maxDistance: 200
-        },
-    };
-    request(requestOptions,
-        function (err, response, body) {
-            var i, data;
-            data = JSON.parse(body);
-            if (response.statusCode === 200 && data.length) {
-                for (i = 0; i < data.length; i++) {
-                    data[i].distance = _formatDistance(data[i].distance);
-                }
-            }
-            renderHomepage(req, res, data);
-        });
+    renderHomepage(req, res);
 };
 
 var _formatDistance = function (distance) {
@@ -142,7 +109,8 @@ var renderReviewForm = function (req, res, locDetail) {
         pageHeader: {
             title: 'Review ' + locDetail.name
         },
-        error: req.query.err
+        error: req.query.err,
+        url: req.originalUrl
     });
 };
 module.exports.doAddReview = function (req, res) {
